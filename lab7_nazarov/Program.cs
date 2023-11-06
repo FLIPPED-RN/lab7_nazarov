46c574b189f85d3d1bcba11e0b127cf261489aa4
Nazarov Ruslan <rnazarov142@gmail.com>
2023-11-07 00:38:14 +0200

    Добавьте файлы проекта.

---
+++ lab7_nazarov/Program.cs
@@ -0,0 +1,16 @@
+﻿//lab7 Вариант 8, средний уровень
+
+Console.Write("Введите вещественное число A (>1): ");
+double A = double.Parse(Console.ReadLine());
+
+int N = 1;
+double sum = 1.0;
+
+while (sum <= A)
+{
+    N++;
+    sum += 1.0 / N;
+}
+
+Console.WriteLine($"Наименьшее N: {N}");
+Console.WriteLine($"Сумма: {sum:F2}");
